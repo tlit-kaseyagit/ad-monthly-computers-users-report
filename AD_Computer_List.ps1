6f7cda3ae1b6c9013cce5a4cec535e2d5e4ba74e
tlit-kaseyagit <97968406+tlit-kaseyagit@users.noreply.github.com>
2022-04-11 10:04:38 -0400

    Add files via upload

---
+++ AD_Computer_List.ps1
@@ -0,0 +1,14 @@
+ $ENV:ComputerAge = 90
+$age = (get-date).AddDays(-$ENV:ComputerAge)
+$DomainCheck = Get-CimInstance -ClassName Win32_OperatingSystem
+New-Item -Path 'C:\TeamLogicIT' -ItemType Directory
+if ($DomainCheck.ProductType -ne "2") { write-output "Invalid" | Out-File C:\TeamLogicIT\AD_Computer_List.txt ; exit 0 }
+$OldComputers = Get-ADComputer -Filter * -properties cn,Enabled,LastLogonDate | select Name,Enabled,LastLogonDate | Where-Object {$_.LastLogonDate -lt $age -and $_.Enabled -eq "True"}
+
+
+if (!$OldComputers) {
+    write-output "No computers inactive for 90 days" | Out-File C:\TeamLogicIT\AD_Computer_List.txt
+}
+else {
+    write-output "Please Review, computers inactive for 90+ days" @($OldComputers) | Out-File C:\TeamLogicIT\AD_Computer_List.txt
+}
---
+++ AD_User_List.ps1
@@ -0,0 +1,26 @@
+ $ENV:UserAge = 30
+
+$age = (get-date).AddDays(-$ENV:UserAge)
+
+$DomainCheck = Get-CimInstance -ClassName Win32_OperatingSystem
+
+if ($DomainCheck.ProductType -ne "2") { write-output "Invalid" | Out-File C:\TeamLogicIT\AD_User_List.txt ; exit 0 }
+
+$OldUsers = Get-ADuser -Filter * -properties Name, Enabled, LastLogonDate | select Name, Enabled, LastLogonDate | Where-Object { $_.LastLogonDate -lt $age -and $_.Enabled -eq "True"}
+
+
+
+
+
+if (!$OldUsers) {
+
+    write-output "No users inactive for 30 days" | Out-File C:\TeamLogicIT\AD_User_List.txt
+
+}
+
+else {
+
+       
+    write-output "Please Review, users inactive for 30+ days" @($OldUsers) | Out-File C:\TeamLogicIT\AD_User_List.txt
+
+} 
