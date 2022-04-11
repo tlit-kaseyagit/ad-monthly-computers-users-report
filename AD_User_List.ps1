26279de65a6228b23702db97be1a9a44b35e0ae2
tlit-kaseyagit <97968406+tlit-kaseyagit@users.noreply.github.com>
2022-04-11 10:09:57 -0400

    Update AD_User_List.ps1
    Added Description Field

--- AD_User_List.ps1
+++ AD_User_List.ps1
@@ -6,7 +6,7 @@ $DomainCheck = Get-CimInstance -ClassName Win32_OperatingSystem
 
 if ($DomainCheck.ProductType -ne "2") { write-output "Invalid" | Out-File C:\TeamLogicIT\AD_User_List.txt ; exit 0 }
 
-$OldUsers = Get-ADuser -Filter * -properties Name, Enabled, LastLogonDate | select Name, Enabled, LastLogonDate | Where-Object { $_.LastLogonDate -lt $age -and $_.Enabled -eq "True"}
+$OldUsers = Get-ADuser -Filter * -properties Name, Enabled, LastLogonDate, Description | select Name, Enabled, LastLogonDate, Description | Where-Object { $_.LastLogonDate -lt $age -and $_.Enabled -eq "True"}
 
 
 
