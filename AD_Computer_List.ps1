85dace9bfc33c5bc2c8e8d1d3aee43d27372e77d
tlit-kaseyagit <97968406+tlit-kaseyagit@users.noreply.github.com>
2022-04-11 10:09:17 -0400

    Update AD_Computer_List.ps1
    Added Description Field

--- AD_Computer_List.ps1
+++ AD_Computer_List.ps1
@@ -3,7 +3,7 @@ $age = (get-date).AddDays(-$ENV:ComputerAge)
 $DomainCheck = Get-CimInstance -ClassName Win32_OperatingSystem
 New-Item -Path 'C:\TeamLogicIT' -ItemType Directory
 if ($DomainCheck.ProductType -ne "2") { write-output "Invalid" | Out-File C:\TeamLogicIT\AD_Computer_List.txt ; exit 0 }
-$OldComputers = Get-ADComputer -Filter * -properties cn,Enabled,LastLogonDate | select Name,Enabled,LastLogonDate | Where-Object {$_.LastLogonDate -lt $age -and $_.Enabled -eq "True"}
+$OldComputers = Get-ADComputer -Filter * -properties cn,Enabled,LastLogonDate,Description | select Name,Enabled,LastLogonDate,Description | Where-Object {$_.LastLogonDate -lt $age -and $_.Enabled -eq "True"}
 
 
 if (!$OldComputers) {
